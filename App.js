5b19f13e46f913d0bcc678864ee0a204b3cb1e31
Markus Sibakov <markus.sibakov@myy.haaga-helia.fi>
2022-01-25 01:48:06 +0200

    laskin

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [val1, setVal1] = useState('');
+  const [val2, setVal2] = useState('');
+  const [result, setResult] = useState('');
+
+  const plusButtonPressed = () => {
+    setResult(Number(val1) + Number(val2));
+  }
+
+  const minusButtonPressed = () => {
+    setResult(Number(val1) - Number(val2));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Result: {result}</Text>
+        
+      <TextInput
+        style={{width: 200, borderColor:'gray', borderWidth:1 }}
+        onChangeText={text => setVal1(text)}
+        value={val1}
+        keyboardType={'decimal-pad'}
+      />
+      <TextInput
+        style={{width: 200, borderColor:'gray', borderWidth:1 }}
+        onChangeText={text => setVal2(text)}
+        value={val2}
+        keyboardType={'decimal-pad'}
+      />
+      <View style={{flexDirection: 'row', alignItems: 'center', justifyContent:'space-between', margin:20}}>
+        <Button onPress={plusButtonPressed} title="+"/>
+        <Text>     </Text>
+        <Button onPress={minusButtonPressed} title="-"/>
+      </View>
     </View>
   );
 }
